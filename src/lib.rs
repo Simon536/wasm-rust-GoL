db0e417300cc6ff79a56bc8cd6b4e5dcdbf6dce2
SimonG <simon.grunwald@outlook.com>
2022-10-27 19:43:35 +1000

    Added code to randomly init cells

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use std::fmt;
 
+extern crate js_sys;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -91,7 +92,7 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                if js_sys::Math:random() < 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
