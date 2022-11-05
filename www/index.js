e47c8bd4266b1f98c8d3347b96e9222a3641d808
SimonG <simon.grunwald@outlook.com>
2022-11-05 16:16:13 +1000

    Draw all live cells, then all dead cells
    Drawing the cells in batches should improve performance

--- www/index.js
+++ www/index.js
@@ -105,15 +105,34 @@ const drawCells = () => {
   const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
   
   ctx.beginPath();
-  
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
+
+  // Draw live cells
+  ctx.fillStyle = ALIVE_COLOUR;
+  for (let row = 0; row < height; row++){
+    for (let col = 0; col < width; col++){
       const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Alive){
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
   
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOUR
-        : ALIVE_COLOUR;
-  
+  // Draw dead cells
+  ctx.fillStyle = DEAD_COLOUR;
+  for (let row = 0; row < height; row++){
+    for (let col = 0; col < width; col++){
+      const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Dead){
+        continue;
+      }
+
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
         row * (CELL_SIZE + 1) + 1,
